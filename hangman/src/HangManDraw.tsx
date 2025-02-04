d3bc37b4b4a471c91cb9b146d0fb4474c2df2af6
a2022134773 <a2022134773@isec.pt>
2025-02-04 16:35:52 +0000

    HangManDraw css

---
+++ hangman/src/HangManDraw.tsx
@@ -0,0 +1,63 @@
+const HEAD = (
+    <div
+    style={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "100%",
+        border: "10px solid black",
+        position: "absolute",
+        top: "50px",
+        right: "-30px"
+    }}></div>
+)
+
+const BODY = (
+    <div
+    style={{
+        width: "10px",
+        height: "100px",
+        background: "black",
+        position: "absolute",
+        top: "120px",
+        right: 0
+    }}></div>
+)
+
+
+export function HangManDraw() {
+    return <div style={{ position: "relative"}} >
+
+        <div 
+        style={{ 
+            height : "50px",
+            width: "10px",
+            background: "black",
+            top: 0,
+            right: 0,
+            position: "absolute"
+            }}>
+        </div>
+
+        <div 
+        style={{ 
+            height : "10px",
+            width: "200px",
+            background: "black", 
+            marginLeft: "120px"
+            }}>
+        </div>
+        <div 
+        style={{ 
+            height : "400px",
+            width: "10px",
+            background: "black",
+            marginLeft: "120px" }}>
+        </div>
+        <div 
+        style={{ 
+            height : "10px",
+            width: "250px",
+            background: "black" }}>
+        </div>
+    </div>
+}
